e3e5a66acebfc412ed704ba42cad689695130f36
Filip <dev.filip0s@protonmail.com>
2022-07-27 21:12:23 +0200

    Refactoring of `JournalController` class
    `Get()` method was renamed to `GetAllJournals()`.
    List of `Journal`s was taken out of the `GetAllJournals` method and put into the class as static member

--- MyTravelJournal.Api/Controllers/JournalController.cs
+++ MyTravelJournal.Api/Controllers/JournalController.cs
@@ -4,25 +4,25 @@ using MyTravelJournal.Api.Models;
 namespace MyTravelJournal.Api.Controllers;
 
 [ApiController]
-[Route("/api/v1/journal")]
+[Route("/api/journal")]
 public class JournalController : Controller
 {
-    [HttpGet]
-    public async Task<ActionResult<List<Journal>>> Get()
+    private static List<Journal> _journals = new List<Journal>
     {
-        var journals = new List<Journal>
+        new Journal
         {
-            new Journal
-            {
-                Id = 0,
-                Title = "Mount Fuji",
-                Description = "Our trip to Japan ^^",
-                Location = "Japan",
-                Start = new DateTime(2022, 5, 24),
-                End = new DateTime(2022, 5, 27),
-            }
-        };
+            Id = 0,
+            Title = "Mount Fuji",
+            Description = "Our trip to Japan ^^",
+            Location = "Japan",
+            Start = new DateTime(2022, 5, 24),
+            End = new DateTime(2022, 5, 27),
+        }
+    };
 
-        return Ok(journals);
+    [HttpGet]
+    public async Task<ActionResult<List<Journal>>> GetAllJournals()
+    {
+        return Ok(_journals);
     }
 }
