6a9b2f07400e51c3c25487b1b34a3b88286b2fec
Filip <dev.filip0s@protonmail.com>
2022-08-02 14:52:25 +0200

    `Program.cs` setup for database usage

--- MyTravelJournal.Api/Program.cs
+++ MyTravelJournal.Api/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MyTravelJournal.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TravelJournalContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
