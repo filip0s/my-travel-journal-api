06be11caa7f4528bca9c26e2dde794570b7f73c1
Filip <dev.filip0s@protonmail.com>
2022-08-06 21:39:42 +0200

    Added error messages to `UserRegisterRequestDto` props

--- MyTravelJournal.Api/DTOs/UserRegisterRequestDto.cs
+++ MyTravelJournal.Api/DTOs/UserRegisterRequestDto.cs
@@ -5,6 +5,10 @@ namespace MyTravelJournal.Api.DTOs;
 public class UserRegisterRequestDto
 {
     [Required] public string Username { get; set; } = string.Empty;
-    [Required, MinLength(8)] public string Password { get; set; } = string.Empty;
-    [Required, Compare("Password")] public string ConfirmPassword { get; set; } = string.Empty;
+
+    [Required, MinLength(8, ErrorMessage = "Password should have at least 8 characters")]
+    public string Password { get; set; } = string.Empty;
+
+    [Required, Compare("Password", ErrorMessage = "Password confirmation does not match with password")]
+    public string ConfirmPassword { get; set; } = string.Empty;
 }
