52c7596d4487ece8d60ac5c9f75554441568ff4b
Filip <dev.filip0s@protonmail.com>
2022-08-06 21:35:19 +0200

    Added `UserRegisterRequestDto`

---
+++ MyTravelJournal.Api/DTOs/UserRegisterRequestDto.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MyTravelJournal.Api.DTOs;
+
+public class UserRegisterRequestDto
+{
+    [Required] public string Username { get; set; } = string.Empty;
+    [Required, MinLength(8)] public string Password { get; set; } = string.Empty;
+    [Required, Compare("Password")] public string ConfirmPassword { get; set; } = string.Empty;
+}
