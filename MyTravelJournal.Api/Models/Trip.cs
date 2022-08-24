3293b0f05a00273dbd2cac0e1294a5e5c739d99f
Filip <dev.filip0s@protonmail.com>
2022-08-24 17:22:42 +0200

    Dates were made nullable.

--- MyTravelJournal.Api/Models/Trip.cs
+++ MyTravelJournal.Api/Models/Trip.cs
@@ -6,6 +6,6 @@ public class Trip
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string Location { get; set; } = string.Empty;
-    public DateTime Start { get; set; }
-    public DateTime End { get; set; }
+    public DateTime? Start { get; set; }
+    public DateTime? End { get; set; }
 }
