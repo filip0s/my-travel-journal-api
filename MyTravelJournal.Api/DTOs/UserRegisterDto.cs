5b91f312531838fd1545232438fe0f48652b7af2
Filip <dev.filip0s@protonmail.com>
2022-08-24 17:22:24 +0200

    Added e-mail to `User`
    E-mail also added to Dto which is mapped to `User` model

--- MyTravelJournal.Api/DTOs/UserRegisterDto.cs
+++ MyTravelJournal.Api/DTOs/UserRegisterDto.cs
@@ -5,6 +5,7 @@ namespace MyTravelJournal.Api.DTOs;
 public class UserRegisterDto
 {
     [Required] public string Username { get; set; } = string.Empty;
+    [Required, EmailAddress] public string Email { get; set; } = string.Empty;
 
     [Required, MinLength(8, ErrorMessage = "Password should have at least 8 characters")]
     public string Password { get; set; } = string.Empty;
--- MyTravelJournal.Api/Models/User.cs
+++ MyTravelJournal.Api/Models/User.cs
@@ -4,6 +4,7 @@ public class User
 {
     public int UserId { get; set; }
     public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public byte[] PasswordHash { get; set; } = new byte[32];
     public byte[] PasswordSalt { get; set; } = new byte[32];
 }
