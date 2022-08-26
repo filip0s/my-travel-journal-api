bc5b07e33d2c561e4657bbc8868d293ac8f40a0c
Filip <dev.filip0s@protonmail.com>
2022-08-26 18:14:30 +0200

    Implemented `VerifyPasswordHash` method

--- MyTravelJournal.Api/Controllers/UserController.cs
+++ MyTravelJournal.Api/Controllers/UserController.cs
@@ -59,4 +59,12 @@ public class UserController : ControllerBase
         passwordSalt = hmac.Key;
         passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
     }
+
+    private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+    {
+        using var hmac = new HMACSHA512(passwordSalt);
+        var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+        return computedHash.SequenceEqual(passwordHash);
+    }
 }
